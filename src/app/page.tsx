5646d553603a5241010658ce4738adea36b786e1
Abdul Rasheed <abdul.rasheed@example.com>
2025-09-30 18:08:00 +0500

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -122,7 +122,7 @@ export default function HomePage() {
       </section>
 
       <section className='w-full min-h-[485px]'>
-  <Image src="/images/home/section.png" alt="Services" width={1600} height={485} className="w-full h-full object-cover rounded-xl" priority />
+  <Image src="/images/home/section.png" alt="Services" width={1600} height={485} className="w-full h-full object-cover rounded-xl" />
       </section>
  <section className="w-full my-12  py-12">
   <div className="max-w-screen-xl mx-auto px-6 sm:px-12 lg:px-20">
