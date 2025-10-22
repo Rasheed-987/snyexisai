c05a0c6cbc08b1df23a13ef7ecab5901d9946a64
Abdul Rasheed <abdul.rasheed@example.com>
2025-10-22 17:47:43 +0500

    new commit

--- src/app/caseStudiesDetails/[id]/page.tsx
+++ src/app/caseStudiesDetails/[id]/page.tsx
@@ -28,7 +28,20 @@ const CaseStudiesDetailPage = () => {
         if (params?.id) fetchCaseStudy()
     }, [params?.id])
 
-    if (loading) return <div>Loading...</div>
+ 
+if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="text-center ">
+          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600 mx-auto mb-4"></div>
+          <p className="text-gray-600">Loading CaseStudies...</p>
+        </div>
+      </div>
+    )
+  }
+
+
+
     if (error) return <div>Error: {error}</div>
     if (!caseStudy) return <div>No case study found.</div>
 
